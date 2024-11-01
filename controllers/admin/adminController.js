ceed6ecaf3de435ec29dca003221e3d850c441a7
sreehari-vr <vrsreehari61@gmail.com>
2024-11-01 09:30:13 +0530

    productEdit

--- controllers/admin/adminController.js
+++ controllers/admin/adminController.js
@@ -2,13 +2,10 @@ const User = require('../../models/userSchema');
 const bcrypt = require('bcrypt')
 
 const loadAdminLogin = async (req,res) => {
-    try {
-        res.render('adminLogin');
-        
-    } catch (error) {
-        console.log("NO adminLogin found");
-        res.status(500).send("server error");
-    }
+        if (req.session.admin) {
+            return res.redirect('/admin/adminDash');
+        } 
+        res.render("adminLogin",{message:null})
 }
 
 const login = async (req,res) =>{
@@ -36,12 +33,24 @@ const login = async (req,res) =>{
 
     const loadAdminDash = async (req,res)=>{
         try {
-             res.render("adminDash")
+            if (!req.session.admin) {
+                return res.render("adminDash")
+
+            }
         } catch (error) {
             console.error("Error loading admin dashboard:",error)
         } 
     }
 
+    const logout = async (req,res) =>{
+        try {
+            req.session.admin=null
+            res.redirect('/admin/login')
+        } catch (error) {
+            console.error("Logout not working:",error)
+        }
+    }
+
     
 
-module.exports={loadAdminLogin,login,loadAdminDash}
+module.exports={loadAdminLogin,login,loadAdminDash,logout}
