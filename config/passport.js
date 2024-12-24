e27b1582c725a5a6b61bffc6cac5fed2e2df47c7
sreehari-vr <vrsreehari61@gmail.com>
2024-12-24 13:18:34 +0530

    google

--- config/passport.js
+++ config/passport.js
@@ -6,7 +6,7 @@ const env = require("dotenv").config();
 passport.use(new GoogleStrategy({
    clientID:process.env.GOOGLE_CLIENT_ID,
    clientSecret:process.env.GOOGLE_CLIENT_SECRET,
-   callbackURL:'http://localhost:3000/auth/google/callback' 
+   callbackURL:'http://sreehari.shop/auth/google/callback' 
 },
 
 async (accessToken,refreshToken,profile,done)=>{
